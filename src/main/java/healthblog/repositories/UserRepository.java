cd38a580688e1048fbab888074c3795624acb030
Kaloyan Madjunov <k.madjunov@gmail.com>
2018-06-10 23:21:49 +0300

    Delete jpa repositories

--- src/main/java/healthblog/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package healthblog.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import healthblog.models.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-    User findByEmail(String email);
-}
