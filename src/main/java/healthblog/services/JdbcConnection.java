f8954e5f49ce2ddcd14001f8ede38290a9e42a0d
Kaloyan Madjunov <k.madjunov@gmail.com>
2018-06-14 21:27:39 +0300

    Add code to common jdbc connection interface and class because the previous commit was old

--- src/main/java/healthblog/services/JdbcConnection.java
+++ src/main/java/healthblog/services/JdbcConnection.java
@@ -1,4 +1,22 @@
 package healthblog.services;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public interface JdbcConnection {
+    Connection getConnection() throws SQLException;
+
+    void closeConnection(Connection con) throws SQLException;
+
+    PreparedStatement getPreparedStatement(Connection con, String statement) throws SQLException;
+
+    void closePreparedStatement(PreparedStatement preparedStatement) throws SQLException;
+
+    int executeUpdate(PreparedStatement query) throws SQLException;
+
+    ResultSet executeQuery(PreparedStatement query) throws SQLException;
+
+    void closeResultSet(ResultSet resultSet) throws SQLException;
 }
--- src/main/java/healthblog/services/JdbcConnectionImpl.java
+++ src/main/java/healthblog/services/JdbcConnectionImpl.java
@@ -1,4 +1,45 @@
 package healthblog.services;
 
-public class JdbcConnectionImpl {
+import org.springframework.stereotype.Component;
+
+import java.sql.*;
+
+@Component
+public class JdbcConnectionImpl implements JdbcConnection {
+    private static String connectionString = "jdbc:mysql://localhost:3306/health_blog?createDatabaseIfNotExist=true&useSSL=false&useUnicode=yes&characterEncoding=UTF-8";
+
+    @Override
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(connectionString);
+    }
+
+    @Override
+    public void closeConnection(Connection con) throws SQLException {
+        con.close();
+    }
+
+    @Override
+    public PreparedStatement getPreparedStatement(Connection con, String statement) throws SQLException {
+        return con.prepareStatement(statement);
+    }
+
+    @Override
+    public void closePreparedStatement(PreparedStatement preparedStatement) throws SQLException {
+        preparedStatement.close();
+    }
+
+    @Override
+    public int executeUpdate(PreparedStatement query) throws SQLException {
+        return query.executeUpdate();
+    }
+
+    @Override
+    public ResultSet executeQuery(PreparedStatement query) throws SQLException {
+        return query.executeQuery();
+    }
+
+    @Override
+    public void closeResultSet(ResultSet resultSet) throws SQLException {
+        resultSet.close();
+    }
 }
